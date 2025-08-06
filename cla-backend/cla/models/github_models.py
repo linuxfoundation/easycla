1884b5e53e14daac25cc50f14bb7cbb5e57a8abc
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-08-06 13:58:03 +0200

    Address Copilot suggestions
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- cla-backend/cla/models/github_models.py
+++ cla-backend/cla/models/github_models.py
@@ -1675,6 +1675,14 @@ def get_merge_group_commit_authors(merge_group_sha, installation_id=None) -> Lis
 
     return commit_authors
 
+def expand_with_co_authors(commit, pr, installation_id, commit_authors):
+    """
+    Helper to append UserCommitSummary objects for all co-authors to commit_authors list.
+    """
+    co_authors = cla.utils.get_co_authors_from_commit(commit)
+    for co_author in co_authors:
+        commit_authors.append(get_co_author_commits(co_author, commit, pr, installation_id))
+
 
 def get_author_summary(commit, pr, installation_id) -> List[UserCommitSummary]:
     """
@@ -1701,9 +1709,7 @@ def get_author_summary(commit, pr, installation_id) -> List[UserCommitSummary]:
             # check for co-author details
             # issue # 3884
             commit_authors.append(commit_author_summary)
-            co_authors = cla.utils.get_co_authors_from_commit(commit)
-            for co_author in co_authors:
-                commit_authors.append(get_co_author_commits(co_author, commit, pr, installation_id))
+            expand_with_co_authors(commit, pr, installation_id, commit_authors)
             return commit_authors
         except (GithubException, IncompletableObject) as exc:
             cla.log.warning(f"{fn} - PR: {pr}, unable to get commit author summary: {exc}")
@@ -1728,26 +1734,20 @@ def get_author_summary(commit, pr, installation_id) -> List[UserCommitSummary]:
                 )
                 cla.log.debug(f"{fn} - PR: {pr}, {commit_author_summary}")
                 commit_authors.append(commit_author_summary)
-                co_authors = cla.utils.get_co_authors_from_commit(commit)
-                for co_author in co_authors:
-                    commit_authors.append(get_co_author_commits(co_author, commit, pr, installation_id))
+                expand_with_co_authors(commit, pr, installation_id, commit_authors)
                 return commit_authors
             except (GithubException, IncompletableObject) as exc:
                 cla.log.warning(f"{fn} - PR: {pr}, unable to get commit author summary: {exc}")
                 commit_author_summary = UserCommitSummary(commit.sha, None, None, None, None, False, False)
                 cla.log.warning(f"{fn} - PR: {pr}, " f"could not find any commit author for SHA {commit_author_summary}")
                 commit_authors.append(commit_author_summary)
-                co_authors = cla.utils.get_co_authors_from_commit(commit)
-                for co_author in co_authors:
-                    commit_authors.append(get_co_author_commits(co_author, commit, pr, installation_id))
+                expand_with_co_authors(commit, pr, installation_id, commit_authors)
                 return commit_authors
     else:
         cla.log.warning(f"{fn} - PR: {pr}, " f"could not find any commit author for SHA {commit.sha}")
         commit_author_summary = UserCommitSummary(commit.sha, None, None, None, None, False, False)
         commit_authors.append(commit_author_summary)
-        co_authors = cla.utils.get_co_authors_from_commit(commit)
-        for co_author in co_authors:
-            commit_authors.append(get_co_author_commits(co_author, commit, pr, installation_id))
+        expand_with_co_authors(commit, pr, installation_id, commit_authors)
         return commit_authors
 
 
