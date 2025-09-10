0a37851efa9e68d552cd00ae40879e6eb1437b8a
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-09-10 09:35:04 +0200

    New utils to get AWS functions configuration and SSM values
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>
    
    Assisted by [OpenAI](https://platform.openai.com/)
    
    Assisted by [GitHub Copilot](https://github.com/features/copilot)

---
+++ utils/list_ssm_parameters.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -z "$REGION" ]
+then
+  REGION="us-east-2"
+fi
+
+if [ -z "$STAGE" ]
+then
+  STAGE="dev"
+fi
+FILTER=${FILTER:-}
+aws ssm describe-parameters --region "$REGION" --profile "lfproduct-${STAGE}" --query 'Parameters[].Name' --output text |
+  tr '\t' '\n' |
+  sort -u |
+  if [ -n "$FILTER" ]; then
+    grep -i "$FILTER"
+  else
+    cat
+  fi
