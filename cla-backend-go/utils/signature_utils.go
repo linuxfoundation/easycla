2607455aedbc3679d9555cbf71bec0ca17a92519
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-09-24 10:16:14 +0200

    Update test coverage for docs, events, foundation v4 APIs groups, add CI job to run cypress E2E tests, udpate AWS util scripts, add debugging for company/signature ACLs in go v4 APIs
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>
    
    Assisted by [OpenAI](https://platform.openai.com/)
    
    Assisted by [GitHub Copilot](https://github.com/features/copilot)

--- cla-backend-go/utils/signature_utils.go
+++ cla-backend-go/utils/signature_utils.go
@@ -6,12 +6,24 @@ package utils
 import (
 	"github.com/LF-Engineering/lfx-kit/auth"
 	v1Models "github.com/linuxfoundation/easycla/cla-backend-go/gen/v1/models"
+	log "github.com/linuxfoundation/easycla/cla-backend-go/logging"
+	"github.com/sirupsen/logrus"
 )
 
 // CurrentUserInACL is a helper function to determine if the current logged in user is in the specified CLA Manager list
 func CurrentUserInACL(authUser *auth.User, managers []v1Models.User) bool {
+	uname := "(null)"
+	if authUser != nil {
+		uname = authUser.UserName
+	}
+	f := logrus.Fields{
+		"functionName": "utils.signature_utils.CurrentUserInACL",
+		"authUser":     uname,
+	}
+
 	var inACL = false
 	for _, manager := range managers {
+		log.WithFields(f).Debugf("ACL check: %+v", manager)
 		if manager.LfUsername == authUser.UserName {
 			inACL = true
 			break
