47dcd83c9959e01982ef0fdb1fd27ccd920b7a73
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-11-18 12:13:14 +0100

    Address various AIs feedback
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>
    
    Assisted by [OpenAI](https://platform.openai.com/)
    
    Assisted by [GitHub Copilot](https://github.com/features/copilot)

--- cla-backend-go/signatures_timestamp_backfill_dev.sh
+++ cla-backend-go/signatures_timestamp_backfill_dev.sh
@@ -1,3 +1,5 @@
+# Copyright The Linux Foundation and each contributor to CommunityBridge.
+# SPDX-License-Identifier: MIT
 #!/bin/bash
 # source setenv.sh
 go build -o signatures_timestamp_backfill cmd/signatures_timestamp_backfill/main.go && ALLOW_CURRENT_TIME='' DEBUG=true STAGE=dev DRY_RUN=true ./signatures_timestamp_backfill
