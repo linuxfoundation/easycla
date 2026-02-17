274fba9e313763df8996400a1ff202025380613a
Lukasz Gryglicki <lgryglicki@cncf.io>
2026-02-17 07:13:14 +0100

    WIP
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>
    
    Assisted by [OpenAI](https://platform.openai.com/)
    
    Assisted by [GitHub Copilot](https://github.com/features/copilot)

---
+++ utils/set_ssm_logging_params.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# REGION=us-east-1
+# STAGE=dev
+# SET=1 
+# STAGE=dev REGION=us-east-1 SET=1 ./set_ssm_logging_params_dev_py.sh
+if [ -z "$STAGE" ]
+then
+  export STAGE=dev
+fi
+if [ -z "$REGION" ]
+then
+  export REGION=us-east-1
+fi
+if [ ! -z "$SET" ]
+then
+  ./utils/set_ssm_value.sh cla-dd-site-dev '?' String
+  ./utils/set_ssm_value.sh cla-dd-api-key-secret-arn-dev "$(cat ./DD_API_KEY_SECRET_ARN.secret)" String
+  ./utils/set_ssm_value.sh cla-dd-extension-layer-arn-dev '?' String
+  ./utils/set_ssm_value.sh cla-ddb-api-logging-dev true String
+  ./utils/set_ssm_value.sh cla-otel-datadog-api-logging-dev true String
+fi
+
+./utils/get_ssm_value.sh cla-dd-site-dev
+./utils/get_ssm_value.sh cla-dd-api-key-secret-arn-dev
+./utils/get_ssm_value.sh cla-dd-extension-layer-arn-dev
+./utils/get_ssm_value.sh cla-ddb-api-logging-dev
+./utils/get_ssm_value.sh cla-otel-datadog-api-logging-dev
