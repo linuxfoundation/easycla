5481b13ab88ced319eae537af1898a32d2f07865
Lukasz Gryglicki <lgryglicki@cncf.io>
2026-04-21 15:59:41 +0200

    Utils to see how ported APIs behave / flow between v1/v2 -> v4
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>
    
    Assisted by [OpenAI](https://platform.openai.com/)
    
    Assisted by [GitHub Copilot](https://github.com/features/copilot)

--- utils/cleanup.sh
+++ utils/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf .git .gitbook/ cla-backend/auth/bin/
+rm -rf .git .gitbook/ cla-backend/auth/bin/ cla-backend/cryptography-layer.zip 
 find . -iname "*.secret" -exec rm -f "{}" \;
