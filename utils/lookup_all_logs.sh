9b1d90606c1200f3816d341afafef8c882ba7d7f
Lukasz Gryglicki <lgryglicki@cncf.io>
2026-05-07 06:54:41 +0200

    Update logs lookup tool
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>
    
    Assisted by [OpenAI](https://platform.openai.com/)
    
    Assisted by [GitHub Copilot](https://github.com/features/copilot)

--- utils/lookup_all_logs.sh
+++ utils/lookup_all_logs.sh
@@ -6,9 +6,13 @@ then
   echo "  if 'text' = '---' then it returns all logs"
   exit 1
 fi
-STAGE=dev REGION=us-east-1 DEBUG=1 DTFROM="${1}" DTTO="${2}" ./utils/search_aws_log_group.sh 'githubactivity' "${3}" > githubactivity.log
-STAGE=dev REGION=us-east-1 DEBUG=1 DTFROM="${1}" DTTO="${2}" ./utils/search_aws_log_group.sh 'apiv1' "${3}" > v1.log
-STAGE=dev REGION=us-east-1 DEBUG=1 DTFROM="${1}" DTTO="${2}" ./utils/search_aws_log_group.sh 'apiv2' "${3}" > v2.log
-STAGE=dev REGION=us-east-1 DEBUG=1 DTFROM="${1}" DTTO="${2}" ./utils/search_aws_log_group.sh 'api-v3-lambda' "${3}" > v3.log
-STAGE=dev REGION=us-east-2 DEBUG=1 DTFROM="${1}" DTTO="${2}" ./utils/search_aws_log_group.sh 'cla-backend-go-api-v4-lambda' "${3}" > v4.log
+if [ -z "$STAGE" ]
+then
+  export STAGE=dev
+fi
+REGION=us-east-1 DEBUG=1 DTFROM="${1}" DTTO="${2}" ./utils/search_aws_log_group.sh 'githubactivity' "${3}" > githubactivity.log
+REGION=us-east-1 DEBUG=1 DTFROM="${1}" DTTO="${2}" ./utils/search_aws_log_group.sh 'apiv1' "${3}" > v1.log
+REGION=us-east-1 DEBUG=1 DTFROM="${1}" DTTO="${2}" ./utils/search_aws_log_group.sh 'apiv2' "${3}" > v2.log
+REGION=us-east-1 DEBUG=1 DTFROM="${1}" DTTO="${2}" ./utils/search_aws_log_group.sh 'api-v3-lambda' "${3}" > v3.log
+REGION=us-east-2 DEBUG=1 DTFROM="${1}" DTTO="${2}" ./utils/search_aws_log_group.sh 'cla-backend-go-api-v4-lambda' "${3}" > v4.log
 ls -l githubactivity.log v?.log
