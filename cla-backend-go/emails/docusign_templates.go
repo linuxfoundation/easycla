545e99fddc3084c1118926c86cc9f5c6feebbbe2
Łukasz Gryglicki <lgryglicki@cncf.io>
2026-07-21 12:50:18 +0200

    WIP
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>
    
    Assisted by [OpenAI](https://platform.openai.com/)
    
    Assisted by [GitHub Copilot](https://github.com/features/copilot)
    
    Assisted by [Claude](https://claude.ai)

--- cla-backend-go/emails/docusign_templates.go
+++ cla-backend-go/emails/docusign_templates.go
@@ -17,13 +17,13 @@ const (
 	// DocumentSignedTemplate is email template for
 	DocumentSignedICLATemplate = `
 		<p>Hello {{.RecipientName}},</p>
-		<p>This is a notification email from EasyCLA regarding the CLA Group {{.CLAGroupName}}.</p>
+		<p>This is a notification email from EasyCLA regarding the {{if eq .Version "v2"}}CLA Group {{.CLAGroupName}}{{else}}project {{.Project.ExternalProjectName}}{{end}}.</p>
 		<p>The CLA has now been signed. You can download the signed CLA as a PDF <a href="{{.PdfLink}}" target="_blank" alt="ICLA Document Link">here</a>.</p>
 		`
 
 	DocumentSignedCCLATemplate = `
 		<p>Hello {{.RecipientName}},</p>
-		<p>This is a notification email from EasyCLA regarding the CLA Group {{.CLAGroupName}}.</p>
+		<p>This is a notification email from EasyCLA regarding the {{if eq .Version "v2"}}CLA Group {{.CLAGroupName}}{{else}}project {{.Project.ExternalProjectName}}{{end}}.</p>
 		<p>The CLA has now been signed. You can download the signed CLA as a PDF <a href="{{.PdfLink}}" target="_blank" alt="CCLA Document Link">here</a>, or from within the <a href="{{.CorporateConsole}}" target="_blank"> EasyCLA CLA Manager console </a>.</p>
 		`
 )
@@ -36,6 +36,7 @@ func RenderDocumentSignedTemplate(svc EmailTemplateService, claGroupModelVersion
 	}
 
 	params.CLAGroupTemplateParams = claGroupParams
+	params.Version = claGroupModelVersion
 	var template string
 	if params.ICLA {
 		template = DocumentSignedICLATemplate
